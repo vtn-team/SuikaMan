8d84f79b475c67a30a3154229755bc43a50b23fd
Nana_77 <nana77amery@gmail.com>
2022-08-06 11:43:56 +0900

    エネミーのスタン

--- Assets/Okuyama/Enemy.cs
+++ Assets/Okuyama/Enemy.cs
@@ -5,11 +5,12 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField, Tooltip("追いかける相手")] GameObject _target;
     [SerializeField,Tooltip("エネミーのスピード")] float _speed = 0.1f;
+    [SerializeField] StanEnemy _stanEnemy;
     float distance;
     /// <summary>徘徊してほしい場所</summary>
     [SerializeField] Transform[] _wanderingPoint;
     [SerializeField] int destPoint = 0;
-    bool _enemy = false;
+    bool _enemy = true;
    
     void Start()
     {
@@ -18,21 +19,30 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
+        if (_stanEnemy.stop) { return; }
         var playerPos = _target.transform.position;
         distance = Vector3.Distance(this.transform.position, playerPos);
 
-        if (distance < 15)
+        //if (distance < 15)
+        //{
+        //    //targetの方に少しずつ向きが変わる
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target.transform.position - transform.position), 0.3f);
+
+        //    //targetに向かって進む
+        //    transform.position += transform.forward * _speed;
+        //}
+        if(_enemy == true)
         {
+            GotoNextPoint();
+        }
+        if(distance < 15)
+        {
+            _enemy = false;
             //targetの方に少しずつ向きが変わる
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target.transform.position - transform.position), 0.3f);
 
             //targetに向かって進む
             transform.position += transform.forward * _speed;
-        }
-        else
-        {
-            GotoNextPoint();
-            //if(_enemy == false) { GotoNextPoint(); }
         }
     }
 
@@ -44,30 +54,31 @@ public class Enemy : MonoBehaviour
 ;
         //targetの方に少しずつ向きが変わる
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_wanderingPoint[destPoint].position - transform.position), 0.3f);
-         //targetに向かって進む
+        //targetに向かって進む
         transform.position += transform.forward * _speed;
-        //_enemy = false;
 
         //// 配列内の次の位置を目標地点に設定し、
         //// 必要ならば出発地点にもどります
-        //destPoint = (destPoint + 1) % _wanderingPoint.Length;
+        destPoint = (destPoint + 1) % _wanderingPoint.Length;
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject == _target)
-        {
-            Debug.Log($"スイカ男が捕まえた");
-        }
-    }
+   
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Item")
+        if (collision.gameObject.name == "aa")
         {
             // 配列内の次の位置を目標地点に設定し、
             // 必要ならば出発地点にもどります
             destPoint = (destPoint + 1) % _wanderingPoint.Length;
-            //GotoNextPoint();
-
+            GotoNextPoint();
+            _enemy = false;
         }
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log($"スイカ男が捕まえた");
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "aa") { _enemy = true; }
     }
 }
